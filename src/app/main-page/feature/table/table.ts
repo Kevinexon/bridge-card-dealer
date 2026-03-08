e282d43a9d97b63dcb76a077515c473ee4315202
Krsytian <kevinexon@gmail.com>
2026-03-08 20:52:33 +0100

    hand component - basic function and style

--- src/app/main-page/feature/table/table.ts
+++ src/app/main-page/feature/table/table.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { Board } from '../../ui/board/board';
 import { Hand } from '../../ui/hand/hand';
 import { Bidding } from '../../ui/bidding/bidding';
 import { TableService } from '../../utils/table-service';
 
 @Component({
   selector: 'app-table',
-  imports: [Board, Hand, Bidding],
+  imports: [Hand, Bidding],
   providers: [TableService],
   templateUrl: './table.html',
   styleUrl: './table.css',
--- src/app/main-page/ui/hand/hand.ts
+++ src/app/main-page/ui/hand/hand.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hand',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './hand.html',
   styleUrl: './hand.css',
 })
 export class Hand {
-
+  handName = input.required<string>();
+  isDealer = input<boolean>(false);
+  isVulnerable = input<boolean>(false);
 }
